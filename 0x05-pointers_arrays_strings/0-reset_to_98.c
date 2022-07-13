268ab4f45973a365a57e1fc808d8ad5a2172f3ad
Jesutoni Rapheal Oluwafemi <oluwafemirapheal@gmail.com>
2022-07-13 15:12:22 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* reset_to_98 -> a function to update the reference of a pointer
-* @n: pointer to n to be updated
-*/
+ * reset_to_98 - main function.
+ * @n: Pointer with the address value.
+ *
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: none.
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;       
+	*n = 98;
 }
